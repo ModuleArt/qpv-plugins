5ead60a97c40c8603b1035253208745895e7a616
Eugene Volynko <volynko.ua@gmail.com>
2020-09-19 14:27:45 +0300

    Initial upload

---
+++ qpv-blur/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace qpv_blur
+{
+	class Main
+	{
+		public Bitmap blur(Bitmap bmp, string path, bool darkMode, string language)
+		{
+			Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height);
+
+			using (var graphics = Graphics.FromImage(newBmp))
+			{
+				graphics.Clear(Color.Red);
+			}
+
+			return newBmp;
+		}
+	}
+}
